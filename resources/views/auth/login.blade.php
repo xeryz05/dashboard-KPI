dd4597d2e6ab45a42e16ed8b8373ad666b6025b1
xeryz05 <xierradepan01@gmail.com>
2023-10-04 09:36:17 +0700

    tambah show password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,6 +26,12 @@
 
         <!-- Remember Me -->
         <div class="block mt-4">
+            <label for="show_password" class="inline-flex items-center">
+                <input id="show_password" type="checkbox" class="rounded dark:bg-light-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="show_password">
+                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Show Password') }}</span>
+            </label>
+        </div>
+        <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-light-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
@@ -45,3 +51,15 @@
         </div>
     </form>
 </x-guest-layout>
+<script>
+    const showPasswordCheckbox = document.getElementById('show_password');
+    const passwordInput = document.getElementById('password');
+
+    showPasswordCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+</script>
